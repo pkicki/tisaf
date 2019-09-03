cb7c49ee5107264f17ccc4896ca705946fbf895e
Kicajowyfreestyle <piotr.m.kicki@gmail.com>
2019-09-03 20:01:10 +0200

    Updated dimensions to represent something real

--- utils/constants.py
+++ utils/constants.py
@@ -1,7 +1,8 @@
 class Car:
-    #rear_axle_to_front = 3.5
-    rear_axle_to_front = 2.5
-    #rear_axle_to_back = 1.
-    rear_axle_to_back = 0.8
-    #W = 2.
-    W = 1.5
+    """Ford Focus III sedan dimensions"""
+    rear_axle_to_front = 3.543
+    #rear_axle_to_front = 2.5
+    rear_axle_to_back = 0.991
+    #rear_axle_to_back = 0.8
+    W = 2.01
+    #W = 1.5
