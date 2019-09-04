d04c1e0e86e6359ca744754329bb3018dc4ecad7
Kicajowyfreestyle <piotr.m.kicki@gmail.com>
2019-09-04 17:36:05 +0200

    Added Kia dimensions

--- utils/constants.py
+++ utils/constants.py
@@ -1,8 +1,9 @@
 class Car:
-    """Ford Focus III sedan dimensions"""
-    rear_axle_to_front = 3.543
-    #rear_axle_to_front = 2.5
-    rear_axle_to_back = 0.991
-    #rear_axle_to_back = 0.8
-    W = 2.01
-    #W = 1.5
+    # Ford Focus III sedan dimensions
+    #rear_axle_to_front = 3.543
+    #rear_axle_to_back = 0.991
+    #W = 2.01
+    # Kia Rio III hatchback dimensions
+    rear_axle_to_front = 3.375
+    rear_axle_to_back = 0.67
+    W = 1.72
